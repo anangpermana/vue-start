c1a38c0517347e792e1b789f7fdde95185fe94b2
Anang Permana <anang143@gmail.com>
2022-11-17 17:01:40 +0700

    integrasi sbadmin2

--- src/main.js
+++ src/main.js
@@ -4,10 +4,22 @@ import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret, faUserAstronaut, faFolder, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret, faUserAstronaut, faFolder, faAngleDown)
+// import './assets/main.css'
+import './assets/sb-admin-2.css'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
